f7afab2995497b317df2e33052d46549d82d9066
kendono <kendono1234@gmail.com>
2025-04-01 00:18:26 +0900

    sync repo

--- init.sh
+++ init.sh
@@ -1,12 +1,24 @@
 
+# workspace
+
 export WORK=$(pwd)
 
 cd ..
 
+# root (parent of workspace)
+
 export ROOT=$(pwd)
 
+# scripts
+
 export scripts=$ROOT/scripts
-export PATH=$PATH:$scripts
+
+export PATH=$PATH:$scripts/bat # Batch (Windows)
+export PATH=$PATH:$scripts/pwsh # PowerShell (Windows)
+export PATH=$PATH:$scripts/py # Python
+export PATH=$PATH:$scripts/sh # Shell
+
+# path
 
 export snippets=$ROOT/snippets
 export projects=$ROOT/projects
